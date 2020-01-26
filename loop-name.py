98b0c500f2b61127f3c1805ba7068b8f0b42cbb8
ahmadbayhaqi <56214296+Kyuubang@users.noreply.github.com>
2020-01-26 18:52:58 +0700

    Add files via upload

---
+++ loop-name.py
@@ -0,0 +1,16 @@
+while True:
+    nama1 = input("masukkan nama: ")
+    cetak = int(input("seberapa banyak ingin di print: "))
+    i = (1) 
+    while i <= cetak:
+        print (nama1)
+        i += 1
+
+    end = input("apakah ingin melanjutkan [y/n]: ")
+    if end == "y": 
+        continue
+    else :
+        break
+print ("================================")
+print ("PROGRAM BY AHMAD BAYHAQI SAPUTRA")
+print ("================================")
